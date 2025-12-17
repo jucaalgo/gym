3a1dbbd7473eccab3c5140fb646b7228d49420c4
Juan <jucaalgo@gmail.com>
2025-12-17 19:37:18 +0100

    fix(deploy): fix PWA icon 404s and bump v3.5.1

--- vite.config.js
+++ vite.config.js
@@ -21,26 +21,10 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-192x192.png',
+            src: 'jca-logo.png',
             sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
             type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
+            purpose: 'any maskable'
           }
         ]
       }
